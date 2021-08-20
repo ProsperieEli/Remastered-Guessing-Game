6951c13a2639488683df53d42ce60fb4a4bd1731
ProsperieEli <ElijahProsperie@gmail.com>
2021-08-20 14:49:15 -0400

    Added functions

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // import functions and grab DOM elements
+import { compareNumbers, finalNumber } from "./utils.js";
 const myButton = document.getElementById('button');
 const myHints = document.getElementById('hints');
 const highOrLow = document.getElementById('too-high');
@@ -6,8 +7,24 @@ const myChances = document.getElementById('chances');
 const myResults = document.getElementById('results');
 // initialize global state
 let chances = 4;
+let correctNumber = finalNumber();
 
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+myButton.addEventListener('click', () => {
+    let myInput = document.getElementById('userInput');
+    const myguess = compareNumbers(Number(myInput.value), correctNumber);
+
+    if (myInput.value === correctNumber){
+        return myResults.textContent = 'You Win!';
+        
+    } else if (myInput.value !== correctNumber){
+        chances --;
+        myResults.textContent = 'You lose!';
+        
+
+    }
+});
--- utils.js
+++ utils.js
@@ -1,15 +1,15 @@
-export function 
 
-const correctNumber = math.floor(math.random * 18));
-compareNumbers(guess, correctNumber){
+export const finalNumber = () => {
+    return Math.ceil(Math.random() * 20);
+};
+export const compareNumbers = (guess, correctNumber) => {
     if (guess === correctNumber){
-        return myResults.textContent = 'You win!';
+        return 'You win!';
     } else if (guess < correctNumber){
-        myHints.textContent = 'Riddle me this: What is below the age to drink, but after the 90s?';
-        highorLow.textContent = 'Too low.';
-        return myResults.textContent = 'You lose!';
+        return 'Too low';
         
-    } else if (guess > correctNumber){
-        
+    } else {
+        return 'Too high';
+
     }
-}
+};
